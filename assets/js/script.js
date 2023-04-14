0159ade4ab060133b633c312710e17e376a32576
kaylab78 <kaylabackus78@gmail.com>
2023-04-14 16:47:26 -0500

    Add pseudocode for jacasvript

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+// Variables
+
+// When the user chooses a potty option and submits it, a new card shows up with the current date, the time of submission and the choice. If there is already a card for the day, the submitted choice shows up with the correct time. The cards are sorted by date newest to oldest.
