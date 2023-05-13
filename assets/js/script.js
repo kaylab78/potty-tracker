0c8ceb9cddd2c39673628f2a9020c79b5edc3176
kaylab78 <kaylabackus78@gmail.com>
2023-05-13 14:12:14 -0500

    The potty tracker form is now a free form instead of a radio button list.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
-// Variables
-
 // When the user chooses a potty option and submits it, a new card shows up with the current date, the time of submission and the choice. If there is already a card for the day, the submitted choice shows up with the correct time. The cards are sorted by date newest to oldest.
 
-// The user clicks submit
+// Variables
+
+// When the page is fully loaded, load the previous potty entries
+
+// The user enters the description of the potty type
+
+// The user clicks submit or presses enter
+
+// The new potty entry appears with the date and time
+
+
 
 // If there's no card for the day
 
